1401dd3b234f1b04ac78eb3b5abbe716e40219fa
Thomas Bye Nilsen <tni068@post.uit.no>
2018-06-27 21:19:05 +0200

    added routing for all pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  return res.render('pages/index');
 });
+
+
+/* GET about page. */ 
+router.get('/about', function(req, res, next) {
+  return res.render('pages/about');
+});
+
+
+/* GET live page */
+router.get('/live', function(req, res, next) {
+  return res.render('pages/live');
+});
+
+
+/* GET historic page */
+router.get('/historic', function(req, res, next) {
+  return res.render('pages/historic');
+});
+
 
 module.exports = router;
