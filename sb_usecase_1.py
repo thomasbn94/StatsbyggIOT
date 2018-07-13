eb4305039b52c8f79b802e5b073c2b4acd67a859
Thomas Bye Nilsen <thomasbn94@gmail.com>
2018-07-13 14:27:41 +0200

    Usecase 1 coming along fine. Need more error-handling and support for SMS notifications

--- sb_usecase_1.py
+++ sb_usecase_1.py
@@ -1,12 +1,8 @@
-''' Implements use-case no. 1. The use-case is about keeping track of 
-    which doors are open at a given time. In particular, we want to alert end-users if 
-    certain doors or windows are open when they shouldn't be. 
+''' Implements use-case no. 1. This use-case is about keeping track of 
+    the temperature of the VR3 computer case. 
 '''
 
-import multiprocessing
-import signal
 from threading import Thread
-from sb_process import SB_Process
 from sb_sseStream import SB_SSEClient
 import requests
 import json
@@ -16,55 +12,128 @@ import credentials
 import sys
 import threading
 import queue
+import signal
+
 
 class SB_Usecase_No1(object):
     def __init__(self):
 
-        ''' For this use-case, all we want is to read the temperature '''
-        self.sensor_filter = {
-            "device_types": "temperature",
-            "event_types": "temperature",
-            "kit": "think-evening-finish"
-        }
+        # Get HTTP parameters 
+        self.sensor_filter = self.get_HTTP_parameters()
 
+        # Signal handler and booleans. Usd to kill parent and thread
+        self.interrupted = False
+        self.thread_kill = False
+        signal.signal(signal.SIGINT, self.signal_handler)
+        
+        # Thread-safe FIFO queue
+        self.queue = queue.Queue()
         self.run()
 
 
     def run(self):
         print("Here I am: use-case no. 1")
 
-        ''' Open up a connection and stream data from a DT cloud '''
+        # Open up a connection and stream data from a DT cloud
         sse_client = SB_SSEClient()
         response = sse_client.getResponse(self.sensor_filter)
         client = sseclient.SSEClient(response)
-        
-        ''' Producer thread. Stream data and insert into queue '''
+    
+        # Producer thread. Stream data and insert into queue
         producer_thread = threading.Thread(target=self.stream_data, args=(client, ))
         producer_thread.start()
+
+        # Read the maximum temperature from the config file
+        max_temp = self.get_max_temperature()
+
+        # Perform use-case logic
+        self.surveil_temperature(max_temp)
         producer_thread.join()
+        
 
-        '''
-        data_point = None
-        return
+    ''' Checks temperature reading for illegal values '''
+    def surveil_temperature(self, max_temp):
+
+        ''' Use-case logic '''
         while True:
+            data_point = None
             try:
-                data_point = data_queue.get()
-                print(data_point)
-            except queue.Queue.Empty as e:
-                    print("Queue is empty...")
-        '''
+                data_point = self.queue.get()
+                sensor_temp = data_point['result']['event']['data']['temperature']['value']
+                if sensor_temp > max_temp:
+                    self.send_notification(max_temp, sensor_temp, data_point)
 
+            except queue.Queue.Empty as e:
+                print("Queue is empty...")
+
+            ''' Break out of loop upon interrupt signal '''
+            if self.interrupted == True:
+                self.thread_kill = True
+                break
+
+
+    ''' Send a notification to notify an end-user '''
+    def send_notification(self, expected_temperature, actual_temperature, json_config):
+        timestamp = json_config['result']['event']['data']['temperature']['updateTime']
+        print("Too high temperature. Should be", expected_temperature,". Was", actual_temperature," at", timestamp)
+
+
+
+    ''' Listen for live sensor data and insert them into a FIFO queue '''
     def stream_data(self, client):
-        print("Hola")
+        print("Streaming live sensor data...")
+
         while True:
             for event in client.events():
-                #pprint.pprint(json.loads(event.data))
                 try:
-                    #self.data_queue.put(json.loads(event.data))
-                    pprint.pprint(json.loads(event.data))
+                    self.queue.put(json.loads(event.data))
                 except queue.Queue.Full as e:
                     print("Queue is full. Dropping events...")
                     continue
+
+                ''' Make sure to break out of this infinite loop '''
+                if self.thread_kill == True:
+                    print("Thread killed")
+                    return
+
+
+    ''' Returns the temperature used in this use-case '''
+    def get_max_temperature(self):
+        # Load temperature from config file
+        try:
+            json_config_file = open('sb_usecase_1_configuration.json', 'r')
+            max_temp = json.loads(json_config_file.read())["other_parameters"]["max_temperature"]
+            json_config_file.close()
+        except IOError:
+            print("Could not read or open config file. Exiting...")
+            sys.exit(1)
+        
+        return max_temp
+
+
+
+    ''' Returns the HTTP parameters used in streaming '''
+    def get_HTTP_parameters(self):
+        # Load sensor data as HTTP parameters
+        try:
+            json_config_file = open('sb_usecase_1_configuration.json', 'r')
+            http_parameters = json.loads(json_config_file.read())["http_parameters"]
+            json_config_file.close()
+        except IOError:
+            print("Could not read or open config file. Exiting...")
+            sys.exit(1)
+
+        return http_parameters
+
+
+
+
+    
+    ''' Signal handler '''
+    def signal_handler(self, signal, frame):
+        self.interrupted = True
+
+
 
 
 if __name__ == '__main__':
