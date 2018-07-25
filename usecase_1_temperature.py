da074b8915138fd1c0c059dd4f9772efb6335736
Høyland <Arild.Hoyland@statsbygg.no>
2018-07-25 14:06:34 +0200

    update config path

--- usecase_1_temperature.py
+++ usecase_1_temperature.py
@@ -116,7 +116,7 @@ class usecase_1_temperature(object):
     def get_HTTP_parameters(self):
         # Load sensor data as HTTP parameters
         try:
-            json_config_file = open('sb_usecase_1_configuration.json', 'r')
+            json_config_file = open('usecase_1_temperature.json.json', 'r')
             http_parameters = json.loads(json_config_file.read())["http_parameters"]
             json_config_file.close()
         except IOError:
